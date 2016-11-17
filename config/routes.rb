4684f615d5d90d45e34faf91e852dce3c12c43bf
Mario Goodwin <mr.goodwin@outlook.com>
2016-11-17 08:45:00 -0500

    added booking

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root 'flights#index'
+  resources :flights, only: [:index]
 
+  get 'bookings/new'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
